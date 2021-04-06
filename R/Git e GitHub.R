5932e273408277620b58000db776fab568a09d6d
Alex Nunes de Sousa <alexnuneszo@gmail.com>
2021-04-05 21:40:05 -0300

    Inserindo os comentários do passo a passo de configuração

--- R/Git e GitHub.R
+++ R/Git e GitHub.R
@@ -1,11 +1,17 @@
+#'Minhas credenciais
+credentials::set_github_pat()
+
+#'Criando o Projeto de Aula
 usethis::create_project("Curso R/03_R para Ciência de Dados II")
 
+#' Adicionando o Git
 usethis::use_git()
 
-
+#' Configurando o usuário do Git
 usethis::use_git_config(
   user.name = "Alex Nunes de Sousa",
   user.email = "alexnuneszo@gmail.com"
 )
 
+#' Adicione o GitHub: O comando acima sincroniza a pasta com o GitHub.
 usethis::use_github(private = TRUE)
