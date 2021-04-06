924bbf1f5818845a02301c523caa27854fd3f6a2
Alex Nunes de Sousa <alexnuneszo@gmail.com>
2021-04-05 21:36:01 -0300

    Initial commit

--- R/Git e GitHub.R
+++ R/Git e GitHub.R
@@ -1,1 +1,11 @@
 usethis::create_project("Curso R/03_R para Ciência de Dados II")
+
+usethis::use_git()
+
+
+usethis::use_git_config(
+  user.name = "Alex Nunes de Sousa",
+  user.email = "alexnuneszo@gmail.com"
+)
+
+usethis::use_github(private = TRUE)
